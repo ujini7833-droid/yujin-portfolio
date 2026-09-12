3c9d7fb89bd2c1fcf6579214d3cbcd6f731cc1c9
유딘 <ujini7833@gmail.com>
2026-09-12 18:40:50 +0900

    feat: 헤더 초안 제작 (#3)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,26 @@
-const Header = () => {
-    return <div>Header</div>
+function Header() {
+    return (
+        <header className="header">
+            <div className="header_inner">
+                <a href="#intro" className="logo">
+                    YUJIN
+                </a>
+
+                <nav className="nav">
+                    <a href="#about">About</a>
+                    <a href="#experience">Experience</a>
+                    <a href="#education">Education</a>
+                    <a href="#skills">Skills</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#connect">Connect</a>
+                </nav>
+
+                <a href="https://github.com/ujini7833-droid" className="github-link">
+                    GitHub
+                </a>
+            </div>
+        </header>
+    )
 }
 
-export default Header;
+export default Header
